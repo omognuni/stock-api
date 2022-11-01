6bc365a9052749857e545eb308cbf0886f125cc4
omognuni <melbong0069@gmail.com>
2022-11-01 17:35:06 +0900

    [Test] 다른 유저 계좌 정보 없는지 확인

--- stock/account/tests/test_account_api.py
+++ stock/account/tests/test_account_api.py
@@ -15,14 +15,15 @@ ACCOUNT_URL = reverse('account:account-list')
 def detail_url(account_id):
     return reverse('account:account-detail', args = [account_id])
 
-def create_account(user):
+def create_account(user, **params):
     defaults = {
         'account_number' : 1112222,
         'bank_name': 'test bank',
         'account_name': 'test account',
         'principal': 1000000,        
     }
-
+    defaults.update(params)
+    
     return Account.objects.create(user=user, **defaults)
 
 def create_holding(**params):
@@ -48,7 +49,7 @@ def create_invest(account,holding, **params):
     return Invest.objects.create(**defaults)
 
 class PrivateAPITest(TestCase):
-    
+    '''인증한 사용자 테스트'''
     def setUp(self):
         username = 'testname'
         password = 'testpass'
@@ -79,6 +80,8 @@ class PrivateAPITest(TestCase):
         
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+        
+        # 자산 확인
         self.assertEqual(res.data[0]['assets'], assets)
             
     def test_retrieve_account_details(self):
@@ -93,6 +96,9 @@ class PrivateAPITest(TestCase):
         assets = invest1.holding_number * invest1.holding.holding_price + \
             invest2.holding_number * invest2.holding.holding_price
         
+        total_earnings = assets - account.principal
+        earnings_rate = total_earnings / account.principal * 100
+        
         url = detail_url(account.id)
         res = self.client.get(url)
         serializer = AccountDetailSerializer(account)
@@ -100,5 +106,22 @@ class PrivateAPITest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
         self.assertEqual(res.data['assets'], assets)
+        self.assertEqual(res.data['total_earnings'], total_earnings )
+        self.assertEqual(res.data['earnings_rate'], earnings_rate)
         
-    
+        
+        
+    def test_retrieve_other_user_account_fail(self):
+        '''다른 유저의 account 정보 없는지 확인'''
+        create_account(user=self.user)
+        user2 = get_user_model().objects.create_user(username='testname2', password='testpass')
+        create_account(user=user2)
+        
+        res = self.client.get(ACCOUNT_URL)
+        
+        accounts = Account.objects.filter(user=self.user)
+        serializer = AccountSerializer(accounts, many=True)
+        
+        self.assertEqual(res.data, serializer.data)
+        
+        
