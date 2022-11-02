769ae6761b065e038739ba2d89b0e49a4d39945f
omognuni <melbong0069@gmail.com>
2022-11-02 12:57:31 +0900

    [Feat] 인증 방식 추가

--- stock/stock/settings.py
+++ stock/stock/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'rest_framework',
+    'rest_framework.authtoken',
     'stock',
     'core',
     'invest',
     'account',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+       'rest_framework.permissions.IsAuthenticated',
+   )
+}
--- stock/stock/urls.py
+++ stock/stock/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/invest', include('invest.urls')),
-    path('api/account', include('account.urls')),
+    path('api/invest/', include('invest.urls')),
+    path('api/account/', include('account.urls')),
+    path('api/user/', include('user.urls')),
 ]
