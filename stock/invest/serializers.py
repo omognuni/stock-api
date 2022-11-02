a70bf25c87f33d1a569d6384da899205c3d036cb
omognuni <melbong0069@gmail.com>
2022-11-02 23:41:47 +0900

    [Feat] 평가금액 추가

--- stock/invest/serializers.py
+++ stock/invest/serializers.py
@@ -7,9 +7,13 @@ class InvestSerializer(serializers.ModelSerializer):
     ISIN = serializers.CharField(source='holding.ISIN', read_only=True)
     holding_name = serializers.CharField(source='holding.holding_name', read_only=True)
     holding_type = serializers.CharField(source='holding.holding_type', read_only=True)
-    holding_price = serializers.CharField(source='holding.holding_price', read_only=True)
+    value = serializers.SerializerMethodField(method_name='get_value')
     
     class Meta:
         model = Invest
-        fields = ['id', 'ISIN', 'holding_name', 'holding_type', 'holding_price']
+        fields = ['id', 'ISIN', 'holding_name', 'holding_type', 'value']
         read_only_fields = fields
+
+    def get_value(self, obj):
+        value = obj.holding_number * obj.holding.holding_price
+        return value
