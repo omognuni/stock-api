9c5206eb71da8ce5a3620d8c0f52b3c581260d0f
omognuni <melbong0069@gmail.com>
2022-11-02 23:16:53 +0900

    [Change] script 파일 이름 및 구조 변경

--- stock/scripts/load_csv.py
+++ stock/scripts/load_csv.py
@@ -8,10 +8,14 @@ import csv
 import os
 
 
-def run():
+def load_csv():
     fhand = open(os.path.join(settings.DATA_ROOT, 'account_asset_info_set.csv'))
     reader = csv.reader(fhand)
+    count = 0
     for row in reader:
+        count += 1
+        if count == 1:
+            continue
         row = [line.strip() for line in row]
         name, bank_name, account_number, account_name, ISIN, holding_price, holding_number = row
         try:
@@ -21,7 +25,7 @@ def run():
             else:
                 user = get_user_model().objects.get(username=account_number)
             account, created = Account.objects.get_or_create(user=user, bank_name=bank_name, account_name=account_name, account_number=account_number)
-            holding, created = Holding.objects.update_or_create(ISIN=ISIN, defaults={holding_price:int(holding_price)})
+            holding, created = Holding.objects.update_or_create(ISIN=ISIN, defaults={'holding_price':int(holding_price)})
             invest = Invest.objects.create(account=account, holding=holding, holding_number=int(holding_number))
         except Exception as e:
             print('Error', e)
@@ -29,7 +33,11 @@ def run():
             
     fhand = open(os.path.join(settings.DATA_ROOT, 'account_basic_info_set.csv'))
     reader = csv.reader(fhand)
+    count = 0
     for row in reader:
+        count += 1
+        if count == 1:
+            continue
         row = [line.strip() for line in row]
         account_number, principal = row
         try:
@@ -47,7 +55,11 @@ def run():
 
     fhand = open(os.path.join(settings.DATA_ROOT, 'asset_group_info_set.csv'))
     reader = csv.reader(fhand)
+    count = 0
     for row in reader:
+        count += 1
+        if count == 1:
+            continue
         row = [line.strip() for line in row]
         holding_name, ISIN, holding_type = row
         holding = Holding.objects.update_or_create(ISIN=ISIN, defaults = {'holding_name': holding_name, 'holding_type': holding_type})
---
+++ stock/scripts/runloadcsv.py
@@ -0,0 +1,4 @@
+from scripts.load_csv import load_csv
+
+def run():
+    load_csv()
