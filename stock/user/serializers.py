48ab92de0a70c9cf045b308aa44ebd9bae47ce7d
omognuni <melbong0069@gmail.com>
2022-11-02 12:58:21 +0900

    [Feat] User 추가

---
+++ stock/user/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+# from django.utils.translation import gettext as _
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    '''user object serializer'''
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        '''user 생성 및 비밀번호 암호화'''
+        user = get_user_model().objects.create_user(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        '''user 업데이트'''
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
---
+++ stock/user/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    '''user 생성'''
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class CreateTokenView(ObtainAuthToken):
+    '''user token 생성'''
+    serializer_class = AuthTokenSerializer
+    permission_classes = [permissions.AllowAny]
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
