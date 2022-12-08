8ec18e9329aac3407e330b210e8a12231188484c
omognuni <melbong0069@gmail.com>
2022-12-08 11:16:22 +0900

    [Feat] 각 api 별 admin 추가

--- stock/core/admin.py
+++ stock/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+admin.site.register(models.Account)
+admin.site.register(models.Invest)
+admin.site.register(models.Holding)
